8b0d36aa0a857f8b0e536346d151b202332402c3
FBL <fabien.bardouil@gmail.com>
2025-12-05 15:09:32 +0100

    Ajout d'une barre de chargement

--- src/Scripts/build_index.py
+++ src/Scripts/build_index.py
@@ -4,29 +4,32 @@ import pandas as pd
 from config.config import MISTRAL_API_KEY,EMBEDDING_MODEL,EMBEDDINGS_DATA
 import time
 import json
-import logging
+from tqdm import tqdm
 
 df = pd.read_csv(PROCESSED_DATA)
 
 embedder = Embedder(api_key=MISTRAL_API_KEY,model=EMBEDDING_MODEL)
 embeddings:list =[]
-for idx, row in df.iterrows():
-    text = f"{row['title.fr']}. {row['description.fr']}"
-    # print(text)
+
+for idx, row in tqdm(enumerate(df.iterrows()), total=len(df), desc="Traitement des événements"):
+    text = f"{row[1]['title.fr']}. {row[1]['description.fr']}"
     chunks, vectors = embedder.chunk_and_embed(text=text)
 
-    for chunk, vector in zip(chunks,vectors):
-        embeddings.append({'event_id': int(row['event_id']),
-                           'chunk':chunk,
-                           'vector':vector.tolist(),
-                           'title':row['title.fr'],
-                           'keywords':row['keywords.fr'],
-                           'start_date':row['firstTiming.begin'],
-                           'end_date':row['lastTiming.end'],
-                           'location_adress':row['location.address'],
-                           'location_name':row['location.name']})
+    for chunk, vector in zip(chunks, vectors):
+        embeddings.append({
+            'event_id': int(row[1]['event_id']),
+            'chunk': chunk,
+            'vector': vector.tolist(),
+            'title': row[1]['title.fr'],
+            'keywords': row[1]['keywords.fr'],
+            'start_date': row[1]['firstTiming.begin'],
+            'end_date': row[1]['lastTiming.end'],
+            'location_adress': row[1]['location.address'],
+            'location_name': row[1]['location.name']
+        })
+
     
-    print(f"Evenement n°{idx} a été traité")
+    # print(f"Evenement n°{idx} a été traité")
 
 with open(EMBEDDINGS_DATA,'w',encoding='utf-8') as f :
     json.dump(embeddings,f,ensure_ascii=False,indent=2)
